d1dd05b4bea2207fb5984bec732a8dd320194eac
Bichchuran10 <bichchuran10@gmail.com>
2023-05-28 13:28:26 +0530

    rtm

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const appid = "your app id";
-
+const appid = "6d20097619b64660aae9eb3256874ea8";
+import AgoraRTC from "agora-rtc-sdk-ng";
+import AgoraRTM from "agora-rtm-sdk";
 const token = null;
 const rtcUid = Math.floor(Math.random() * 2032);
+const rtmUid = String(Math.floor(Math.random() * 2032));
 
 let roomId = "main";
 
@@ -13,11 +15,24 @@ let audioTracks = {
 let micMuted = true;
 
 let rtcClient;
+let rtmClient;
+let channel;
+
+const initRtm = async (name) => {
+  rtmClient = AgoraRTM.createInstance(appid);
+  await rtmClient.login({ uid: rtmUid, token: token });
+  channel = rtmClient.createChannel(roomId);
+  await channel.join();
+  getChannelMembers();
+  channel.on("MemberJoined", handleMemberJoined);
+  channel.on("MemberLeft", handleMemberLeft);
+  window.addEventListener("beforeunload", leaveRTMChannel);
+};
 
 const initRtc = async () => {
   rtcClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
-  rtcClient.on("user-joined", handleUserJoined);
+  //   rtcClient.on("user-joined", handleUserJoined);
   rtcClient.on("user-published", handleUserPublished);
   rtcClient.on("user-left", handleUserLeft);
 
@@ -26,14 +41,14 @@ const initRtc = async () => {
   audioTracks.localAudioTrack.setMuted(micMuted);
   await rtcClient.publish(audioTracks.localAudioTrack);
 
-  document
-    .getElementById("members")
-    .insertAdjacentHTML(
-      "beforeend",
-      `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`
-    );
+  //   document
+  //     .getElementById("members")
+  //     .insertAdjacentHTML(
+  //       "beforeend",
+  //       `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`
+  //     );
 
-  initVolumeIndicator();
+  //   initVolumeIndicator();
 };
 
 let initVolumeIndicator = async () => {
@@ -62,15 +77,15 @@ let initVolumeIndicator = async () => {
   });
 };
 
-let handleUserJoined = async (user) => {
-  console.log("USER:", user);
-  document
-    .getElementById("members")
-    .insertAdjacentHTML(
-      "beforeend",
-      `<div class="speaker user-rtc-${user.uid}" id="${user.uid}"><p>${user.uid}</p></div>`
-    );
-};
+// let handleUserJoined = async (user) => {
+//   console.log("USER:", user);
+//   document
+//     .getElementById("members")
+//     .insertAdjacentHTML(
+//       "beforeend",
+//       `<div class="speaker user-rtc-${user.uid}" id="${user.uid}"><p>${user.uid}</p></div>`
+//     );
+// };
 
 let handleUserPublished = async (user, mediaType) => {
   await rtcClient.subscribe(user, mediaType);
@@ -83,7 +98,37 @@ let handleUserPublished = async (user, mediaType) => {
 
 let handleUserLeft = async (user) => {
   delete audioTracks.remoteAudioTracks[user.uid];
-  document.getElementById(user.uid).remove();
+  //   document.getElementById(user.uid).remove();
+};
+
+let handleMemberJoined = async (MemberId) => {
+  document
+    .getElementById("members")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<div class="speaker user-rtc-${"---"}" id="${MemberId}"><p>${MemberId}</p></div>`
+    );
+};
+
+let handleMemberLeft = async (MemberId) => {
+  document.getElementById(MemberId).remove();
+};
+
+let getChannelMembers = async () => {
+  //1
+  let members = await channel.getMembers();
+
+  //2
+  for (let i = 0; members.length > i; i++) {
+    let newMember = `
+      <div class="speaker user-rtc-${"-----"}" id="${members[i]}">
+          <p>${members[i]}</p>
+      </div>`;
+
+    document
+      .getElementById("members")
+      .insertAdjacentHTML("beforeend", newMember);
+  }
 };
 
 const toggleMic = async (e) => {
@@ -104,17 +149,25 @@ let lobbyForm = document.getElementById("form");
 
 const enterRoom = async (e) => {
   e.preventDefault();
+
+  let displayName = "";
   initRtc();
+  initRtm();
 
   lobbyForm.style.display = "none";
   document.getElementById("room-header").style.display = "flex";
 };
-
+let leaveRTMChannel = async () => {
+  await channel.leave();
+  await rtmClient.logout();
+};
 let leaveRoom = async () => {
   audioTracks.localAudioTrack.stop();
   audioTracks.localAudioTrack.close();
   rtcClient.unpublish();
   rtcClient.leave();
+
+  leaveRTMChannel();
 
   document.getElementById("form").style.display = "block";
   document.getElementById("room-header").style.display = "none";
